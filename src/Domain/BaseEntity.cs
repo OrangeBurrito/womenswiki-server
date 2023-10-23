9c1ceceeb15d704671bd4f7b33fba43d50ad1cfb
OrangeBurrito <root@orangeburrito.com>
2023-10-23 21:27:03 +0100

    refactor: move endpoints to separate class

--- src/Domain/BaseEntity.cs
+++ src/Domain/BaseEntity.cs
@@ -1,4 +1,6 @@
-public class BaseEntity {
+namespace WomensWiki.Domain;
+
+public abstract class BaseEntity {
     public Guid Id { get; private set; } = Guid.NewGuid();
     public DateTimeOffset CreatedAt { get; private set; } = DateTimeOffset.UtcNow;
 }
--- src/Program.cs
+++ src/Program.cs
@@ -1,11 +1,8 @@
-using Microsoft.EntityFrameworkCore;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<WikiContext>(options => {
     options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres"));
 });
-
 builder.Services.AddCors(options => {
     options.AddPolicy("WikiPolicy", policy => {
         var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>();
@@ -21,38 +18,5 @@ builder.Services.AddCors(options => {
 var app = builder.Build();
 app.UseCors("WikiPolicy");
 
-app.MapGet("/", () => "Hello World!");
-
-app.MapGet("/articles", (WikiContext wikiContext) => {
-    return wikiContext.Articles;
-});
-
-app.MapGet("/article/{id}", (WikiContext wikiContext, Guid id) => {
-    Console.WriteLine(id);
-    return wikiContext.Articles.Find(id);
-});
-
-app.MapPost("/article/create", async (WikiContext wikiContext, HttpContext httpContext) => {
-    var title = httpContext.Request.Form["title"].ToString();
-    var article = new Article(title);
-
-    await wikiContext.Articles.AddAsync(article);
-    await wikiContext.SaveChangesAsync();
-    return article;
-});
-
-app.MapPatch("/article/update/{id}", async (WikiContext wikiContext, Guid id, string Title) => {
-    var article = await wikiContext.Articles.FindAsync(id);
-    article.Update(Title);
-    await wikiContext.SaveChangesAsync();
-    return article;
-});
-
-app.MapDelete("/article/delete/{id}", async (WikiContext wikiContext, Guid id) => {
-    var article = await wikiContext.Articles.FindAsync(id);
-    wikiContext.Articles.Remove(article);
-    await wikiContext.SaveChangesAsync();
-    return article;
-});
-
+app.MapEndpoints();
 app.Run();
