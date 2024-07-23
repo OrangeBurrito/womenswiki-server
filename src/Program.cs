1e107688cc54af98ad527ffb604532287e750a25
OrangeBurrito <root@orangeburrito.com>
2024-07-23 17:23:38 +0700

    fix: incorrect azure connection string

--- src/Program.cs
+++ src/Program.cs
@@ -5,7 +5,7 @@ using FluentValidation;
 var builder = WebApplication.CreateBuilder(args);
 
 var assembly = typeof(Program).Assembly;
-var connectionString = builder.Environment.IsDevelopment() ? "DevConnection" : "AzureConnection";
+var connectionString = builder.Environment.IsDevelopment() ? "DevConnection" : "SQLCONNSTR_AzureConnection";
 
 builder.Services.AddDbContext<AppDbContext>(o => o.UseSqlServer(builder.Configuration.GetConnectionString(connectionString)));
 builder.Services.AddMediatR(c => c.RegisterServicesFromAssembly(assembly));
