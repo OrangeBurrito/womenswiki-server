e87b26926d2aeff19735f70995a74f6ffdbf9d01
OrangeBurrito <root@orangeburrito.com>
2024-07-25 00:59:11 +0700

    feat: add articleBySlug query

---
+++ src/Features/Articles/GetArticleBySlug.cs
@@ -0,0 +1,25 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using WomensWiki.Common;
+using WomensWiki.Contracts;
+
+namespace WomensWiki.Features.Articles;
+
+public static class GetArticleBySlug {
+    public record GetArticleBySlugRequest(string Slug) : IRequest<ArticleResponse>;
+       internal sealed class GetArticleBySlugHandler(AppDbContext dbContext) : IRequestHandler<GetArticleBySlugRequest, ArticleResponse> {
+        public async Task<ArticleResponse> Handle(GetArticleBySlugRequest request, CancellationToken cancellationToken) {
+            var article = await dbContext.Articles.FirstOrDefaultAsync(a => a.Slug == request.Slug);
+            
+            return ArticleResponse.FromArticle(article);
+        }
+    }
+
+    [QueryType]
+    public class GetArticleBySlugQuery {
+        public async Task<ArticleResponse> GetArticleBySlugAsync([Service] ISender sender, string slug) {
+            var input = new GetArticleBySlugRequest(slug);
+            return await sender.Send(input);
+        }
+    }
+}
