5de6f81e63760b8692aefa6215f91de236fdd81a
OrangeBurrito <root@orangeburrito.com>
2024-08-19 00:37:54 +0700

    fix: cors missing for wiki

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using WomensWiki.Common;
 using FluentValidation;
-using HotChocolate.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,12 +11,15 @@ builder.Services.AddDbContext<AppDbContext>(o => o.UseSqlServer(builder.Configur
 builder.Services.AddMediatR(c => c.RegisterServicesFromAssembly(assembly));
 builder.Services.AddValidatorsFromAssembly(assembly);
 builder.Services.AddCors(o => o.AddPolicy("Localhost", p => { p.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader(); }));
+builder.Services.AddCors(o => o.AddPolicy("Wiki", p => { p.WithOrigins("https://www.womenswiki.org").AllowAnyMethod().AllowAnyHeader(); }));
 builder.Services.AddGraphQLServer().AddTypes().AddSorting();
 
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment()) {
     app.UseCors("Localhost");
+} else {
+    app.UseCors("Wiki");
 }
 
 app.MapGraphQL();
